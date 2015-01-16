a6c9d0849179479670a6645b878fa5b20000e7dd
wesley <wesley@wesley-lp>
2015-01-16 14:57:37 +0100

    simple roomFactory to return undecorated room

--- src/main/java/edu/avans/hartigehap/domain/RoomFactory.java
+++ src/main/java/edu/avans/hartigehap/domain/RoomFactory.java
@@ -1,5 +1,15 @@
 package edu.avans.hartigehap.domain;
 
+import java.util.List;
+
 public class RoomFactory {
+	
+	public IRoom buildRoom(List<String> additions) {
+		IRoom room = new Room("testRoom", 5);
+		
+		
+		return room;
+		
+	}
 
 }
